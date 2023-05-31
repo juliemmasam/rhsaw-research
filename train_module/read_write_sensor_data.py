059dcb47f0a1cb8938f1c41c24df0d1dfc1b62b6
Emmanuel Julius Samson <juliemmasam@gmail.com>
2023-05-31 14:36:00 -0500

    Added a working code for the location and speed sensor module.

--- train_module/read_write_sensor_data.py
+++ train_module/read_write_sensor_data.py
@@ -5,6 +5,11 @@ from datetime import datetime
 
 DATA_FILE = "/home/train/train_data/train_data.json"
 
+# Initialize the gps session
+session = gps.gps()
+session.stream(gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
+
+
 def read_sensor_data():
     '''
     The code written in this part is required to read the location
@@ -14,48 +19,49 @@ def read_sensor_data():
     In this first version of the code, we have considered the 
     Adafruit Ultimate GPS sensor
     '''
-    session = gps.gps("localhost", "2947")
-    session.stream(gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
 
-    location = "latitude, longitude"
-    speed = "speed in mph"
+    # Initialize necessary variables
     timestamp = datetime.now()
+    report = {"class": None}
+    report_has_data = False
+
+    # Initialize the location and speed data
+    location = None
+    latitude = None
+    longitude = None
+    speed = None
 
     try: 
         report = session.next()
-        if report["class"] == "TPV":
-            if hasattr(report, "latitude"):
-                latitude = report.latitude
-            else:
-                latitude = None
-            
-            if hasattr(report, "longitude"):
-                longitude = report.longitude
-            else: 
-                longitude = None
+        
+        print(report)
+    except:
+        quit()
 
-            if hasattr(report, "speed"):
-                speed = report.speed
-            else:
-                speed = None
 
-            location = {"latitude": latitude, "longitude": longitude}
-                 
-    except KeyError:
-        pass
-    except KeyboardInterrupt:
-        pass
-    except StopIteration: 
-        session = None
-        print("GPSD has terminated")
+    if report["class"] == "TPV":
+        report_has_data = True
+        if hasattr(report, "lat"):
+            latitude = report.lat
+        
+        if hasattr(report, "lon"):
+            longitude = report.lon
+
+        if hasattr(report, "speed"):
+            speed = report.speed
+        
+        location = {"latitude": latitude, "longitude": longitude}
     
+    else:
+        location = {}
+
     current_loc_speed = {
         "location": location, 
         "speed": speed,
         "timestamp": timestamp.strftime("%m-%d-%y, %H:%M:%S")
         }
 
-    return current_loc_speed
+    return (report_has_data, current_loc_speed)
 
 
 def read_data_file(): 
@@ -73,10 +79,12 @@ def write_loc_speed_data(new_json_data):
 
 
 while True: 
-    current_sensor_data = read_sensor_data()
-    current_json_data = read_data_file()
-    current_json_data["loc_speed"].append(current_sensor_data)
-    write_loc_speed_data(current_json_data)
-    print("Data has been written.")
-    time.sleep(30)
+    (has_data, current_sensor_data) = read_sensor_data()
+    if (has_data):
+        current_json_data = read_data_file()
+        current_json_data["loc_speed"].append(current_sensor_data)
+        write_loc_speed_data(current_json_data)
+        print("Data has been written.")
+        time.sleep(1) # Run the program every after one second of sleep
+    
     
