9c8fcf1f8273e60c694e21cccb244cda43e9c059
Emmanuel Julius Samson <juliemmasam@gmail.com>
2023-06-14 13:37:42 -0500

    Add the logic for periodic check and publishing of the data in the cache.

---
+++ train_module/periodic_publish.py
@@ -0,0 +1,56 @@
+# This module is written for periodic check and publishing of the data
+# present in the cache
+
+import publish_data
+import json
+import time
+
+CACHE_FILE = "/home/train/train_data/cache.json"
+
+def open_cache():
+    file = open(CACHE_FILE)
+    cache = json.load(file)
+    file.close()
+
+    if hasattr("cache", cache):
+        return cache
+    else:
+        return "Corrupted File"
+    
+
+def store_unpublished_records_to_cache(unpublished_records):
+    with open(CACHE_FILE, "w") as file:
+        json.dump(unpublished_records, file)
+        file.close()
+    return
+    
+
+def cache_is_empty():
+    cache = open_cache()
+    if(cache != "Corrupted File"):
+        return len(cache["cache"]) == 0
+    else:
+        return True # Later options to log the details of corrupt cache file
+
+
+def publish_cache_records():
+    if(cache_is_empty()):
+         return
+    else:
+        cache = open_cache()
+        
+        for index in range(len(cache["cache"])-1, -1, -1):
+            record = cache["cache"].pop(index) # Get the latest record
+            is_published = publish_data.publish_data(record) # Publish the latest records first (LIFO)
+            if(not is_published):
+                cache["cache"].append(record)
+                unpublished_records = cache["cache"]
+                store_unpublished_records_to_cache(unpublished_records)
+                break # Break the loop when the 
+        return 
+    
+
+while True:
+    publish_cache_records()
+    time.sleep(1200)
+
