ba1322715e97a395d88451d443c5a317853057b0
Emmanuel Julius Samson <juliemmasam@gmail.com>
2023-06-14 16:58:09 -0500

    Added utility methods for reading from and writing to files.

--- train_module/utils.py
+++ train_module/utils.py
@@ -1,2 +1,20 @@
-# This file will contain functions for reading from 
+# This file contains functions for reading from 
 # and writing to files, tasks which seem repetitive now
+import json
+
+def read_from_file(filepath):
+    file = open(filepath)
+    file_contents = json.load(file)
+    file.close()
+
+    return file_contents
+
+
+def write_to_file(filepath, file_contents): 
+    try:
+        with open(filepath, "w") as file: 
+            json.dump(file_contents, file)
+            file.close()
+        return True
+    except:
+        return False
