762199368e0174ba136d2bb0bf6798d1e1853bae
Emmanuel Julius Samson <juliemmasam@gmail.com>
2023-06-15 14:27:50 -0500

    Add database connection logic.

--- central_server/server.js
+++ central_server/server.js
@@ -1,10 +1,18 @@
+const dotenv = require("dotenv")
+const mongoose = require("mongoose")
 const express = require("express")
+
+dotenv.config()
+const DB_URI = process.env.DB_URI
+
 const server = express()
-
 const trains = require("./trains")
-
 server.use('/trains', trains)
 
-server.listen(3000, ()=>{
-    console.log("The server is listening to requests")
+mongoose.connect(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true})
+.then((results) => {
+    console.log("Connection to the database is complete")
+    server.listen(3000, ()=>{
+        console.log("The server is listening to requests")
+    })    
 })
