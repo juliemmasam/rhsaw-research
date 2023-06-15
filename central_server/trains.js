d6f7f9fce354c616d28a0b52b36f53993593bf69
Emmanuel Julius Samson <juliemmasam@gmail.com>
2023-06-15 14:27:17 -0500

    Add routes and database logic for addition of train data.

--- central_server/trains.js
+++ central_server/trains.js
@@ -1,15 +1,33 @@
+const LocSpeed = require("./models/loc_speed")
 const express = require("express")
 const router = express()
 
 router.use(express.json())
 
-router.route("/:id")
+router.route("/new_train")
+    .post((req, res) => {
+        const loc_speed_data = new LocSpeed({
+            train_id: req.body.train_id, 
+            loc_speed: []
+        }) 
+        
+        loc_speed_data.save()
+        .then((result) => res.send(result))
+        .catch((err) => console.log(err))
+    })
+
+
+router.route("/loc_speed_data/:id")
     .get((req, res) => {
         res.send(`The get request for train ${req.params.id} was issued`)
     })
-    .post((req, res) => {
-        console.log(req.body)
-        res.send(`The post request for train ${req.params.id} was issued`)
+    .post((req, res) => {  
+        LocSpeed.updateOne(
+            { train_id: req.body.train_id }, 
+            { $push: { loc_speed: req.body.data } }
+        )
+        .then((result) => res.send(result.acknowledged))
+        .catch((err) => console.log(err))
     })
 
 module.exports = router
